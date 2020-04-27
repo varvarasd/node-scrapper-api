e1937dd097b344eb475ffb249b8b761b385b31fa
varvarasd <silviudanielvarvara@gmail.com>
2020-04-27 20:05:46 +0100

    update trusted req host

--- router.js
+++ router.js
@@ -6,7 +6,7 @@ const jobBoard = process.env.TRUSTED_REQ;
 
 router.get("/jobs", async (req, res, next) => {
   if(req.hostname === jobBoard){
-    res.header("Access-Control-Allow-Origin", "http://" + jobBoard); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", jobBoard); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
     const role = req.query.role;
     const city = req.query.city;
